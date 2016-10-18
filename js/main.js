dadfa462b69514c54c4ee299797da91dad68f397
James Hildreth <hildreth.james@gmail.com>
2016-10-18 13:06:17 -0400

    rework

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 fetch('http://thinksaydo.com/tiyproxy.php?url=' + encodeURIComponent('https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop'))
-  .then(function(response) {
+.then(function(response) {
     return response.json()
-  })
-  .then(function(etsyData) {
+})
+.then(function(etsyData) {
     etsyData.results.forEach(function(boardGame) {
-      var div = document.createElement('a')
-      div.classList.add('board-game')
-      div.setAttribute('href', boardGame.url)
+        var div = document.createElement('div')
+        div.classList.add('board-game', 'col-md-3', 'titleOverflow')
+        //   div.setAttribute('href', boardGame.url)
 
-      var img = document.createElement('img')
-      img.setAttribute('src', boardGame.Images[0].url_570xN)
-      console.log(boardGame)
-      div.appendChild(img)
+        var div2 = document.createElement('div')
+        div2.classList.add('boardgamePic')
+        div2.setAttribute('href', boardGame.url)
+        div.appendChild(div2)
 
-      var title = document.createElement('p')
-      title.innerHTML = boardGame.title
-      div.appendChild(title)
+        var img = document.createElement('img')
+        img.setAttribute('src', boardGame.Images[0].url_570xN)
+        div2.appendChild(img)
 
-      var seller = document.createElement('p')
-      seller.innerHTML = boardGame.Shop.shop_name
-      div.appendChild(seller)
+        var title = document.createElement('h5')
+        title.innerHTML = boardGame.title
+        title.classList.add('titleOverflow')
+        div.appendChild(title)
 
-      var price = document.createElement('p')
-      price.innerHTML = boardGame.price
-      div.appendChild(price)
+        var seller = document.createElement('p')
+        seller.innerHTML = boardGame.Shop.shop_name
+        seller.classList.add('sellerInfo')
+        div.appendChild(seller)
 
-      document.getElementById('etsyData').appendChild(div)
+        var price = document.createElement('p')
+        price.innerHTML = '$' + boardGame.price
+        price.classList.add('priceInfo')
+        div.appendChild(price)
 
+        document.getElementById('etsyData').appendChild(div)
     })
-  })
-  .catch(function(ex) {
+})
+.catch(function(ex) {
     console.log('parsing failed', ex)
-  })
+})
